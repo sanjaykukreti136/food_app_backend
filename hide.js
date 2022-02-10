1d9e689334d27f01faaa8288776768328cd2bff8
unknown <sanjaykukreti900@gmail.com>
2022-02-10 09:54:47 +0530

    plans

--- hide.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    link : 'mongodb+srv://sanjay_1322:jZGxRSXDcFE33TkR@cluster0.1f1th.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    JWT_KEY : "imfirfm323fvvr3e",
-    APP_PASSWORD : "xstrzdbxgiuqvrrh"
- }
--- secrets.js
+++ secrets.js
@@ -1,3 +1,5 @@
-const obj = require('./hide');
-
-module.exports = obj;
+module.exports = {
+    link : 'mongodb+srv://sanjay_1322:jZGxRSXDcFE33TkR@cluster0.1f1th.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    JWT_KEY : "imfirfm323fvvr3e",
+    APP_PASSWORD : "xstrzdbxgiuqvrrh"
+ }
